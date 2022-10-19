044ce3b18acab0ac0037740667f2f9059adda8ac
Viki Val <viktorko99@gmail.com>
2022-10-19 18:07:31 +0200

    :zap: consts are now enums

--- src/consts.ts
+++ src/consts.ts
@@ -1,66 +1,70 @@
-// FOR FUTURE USE
+// FOR FUTURE USE,
 
-// POLKADOT IDS
-export const StatemintID = 1000
-export const AcalaID = 2000
-export const AstarID = 2006
-export const Bifrost_PolkadotID = 2030
-export const CentrifugeID = 2031
-export const CloverID = 2002
-export const Composable_FinanceID = 2019
-export const DarwiniaID = 2046
-export const EfinityID = 2021
-export const EquilibriumID = 2011
-export const HydraDXID = 2034
-export const Integritee_ShellID = 2039
-export const interlayID = 2032
-export const KapexID = 2007
-export const KILT_SpiritnetID = 2086
-export const KylinID = 2052
-export const LitentryID = 2013
-export const MoonbeamID = 2004
-export const NodleID = 2026
-export const OriginTrailID = 2043
-export const ParallelID = 2012
-export const PhalaID = 2035
-export const UNIQUEID = 2037
+// POLKADOT S,
+export enum DotParaId {
+  Statemint = 1000,
+  Acala = 2000,
+  Astar = 2006,
+  Bifrost_Polkadot = 2030,
+  Centrifuge = 2031,
+  Clover = 2002,
+  Composable_Finance = 2019,
+  Darwinia = 2046,
+  Efinity = 2021,
+  Equilibrium = 2011,
+  HydraDX = 2034,
+  Integritee_Shell = 2039,
+  Interlay = 2032,
+  Kapex = 2007,
+  KILT_Spiritnet = 2086,
+  Kylin = 2052,
+  Litentry = 2013,
+  Moonbeam = 2004,
+  Nodle = 2026,
+  OriginTrail = 2043,
+  Parallel = 2012,
+  Phala = 2035,
+  UNIQUE = 2037,
+}
 
-// KUSAMA IDS
-export const BasiliskID = 2090
-export const KaruraID = 2000
-export const PichiuID = 2102
-export const Bifrost_KusamaID = 2001
-export const StatemineID = 1000
-export const EncointerID = 1001
-export const AltairID = 2088
-export const AmplitudeID = 2124
-export const BajunID = 2119
-export const PioneerID = 2096
-export const CalamariID = 2084
-export const Crust_ShadowID = 2012
-export const CrabID = 2105
-export const DorafactoryID = 2115
-export const GMID = 2123
-export const imbueID = 2121
-export const IntegriteeID = 2015
-export const InvArch_TinkerID = 2125
-export const KabochaID = 2113
-export const KhalaID = 2004
-export const KICOID = 2107
-export const kintsugiID = 2092
-export const ListenID = 2118
-export const LitmusID = 2106
-export const MangataID = 2110
-export const MarsID = 2008
-export const MoonriverID = 2023
-export const Parallel_HeikoID = 2085
-export const PicassoID = 2087
-export const QUARTZ_by_UNIQUEID = 2095
-export const RobotomicsID = 2048
-export const ShidenID = 2007
-export const SORAID = 2011
-export const SubsocialXID = 2100
-export const TanganikaID = 2116
-export const TuringID = 2114
-export const UnorthodoxID = -1 // Unable to connect
-export const ZelgeistID = 2101
+// KUSAMA S,
+export enum KusamaParaId {
+  Basilisk = 2090,
+  Karura = 2000,
+  Pichiu = 2102,
+  Bifrost_Kusama = 2001,
+  Statemine = 1000,
+  Encointer = 1001,
+  Altair = 2088,
+  Amplitude = 2124,
+  Bajun = 2119,
+  Pioneer = 2096,
+  Calamari = 2084,
+  Crust_Shadow = 2012,
+  Crab = 2105,
+  Dorafactory = 2115,
+  GM = 2123,
+  imbue = 2121,
+  Integritee = 2015,
+  InvArch_Tinker = 2125,
+  Kabocha = 2113,
+  Khala = 2004,
+  KICO = 2107,
+  kintsugi = 2092,
+  Listen = 2118,
+  Litmus = 2106,
+  Mangata = 2110,
+  Mars = 2008,
+  Moonriver = 2023,
+  Parallel_Heiko = 2085,
+  Picasso = 2087,
+  QUARTZ_by_UNIQUE = 2095,
+  Robotomics = 2048,
+  Shiden = 2007,
+  SORA = 2011,
+  SubsocialX = 2100,
+  Tanganika = 2116,
+  Turing = 2114,
+  Unorthodox = -1, // Unable to connect,
+  Zelgeist = 2101,
+}
