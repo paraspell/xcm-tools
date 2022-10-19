bbb638dcbfea556d8f305bd2366bfc361a37c19a
Viki Val <viktorko99@gmail.com>
2022-10-19 17:58:51 +0200

    :coffin: :label: rmeove unsude types

--- src/types.ts
+++ src/types.ts
@@ -5,21 +5,3 @@ import { SubmittableExtrinsic } from '@polkadot/api/types'
 export type UpdateFunction = (name: string, index: number) => string
 export type Extrinsic = SubmittableExtrinsic<'promise'>
 export type ExtrinsicFunction<T> = (arg: T) => Extrinsic
-
-export type DisplayType = null | 'boost_number' | 'number' | 'boost_percentage'
-
-export type Attribute = {
-  display_type?: DisplayType
-  trait_type?: string
-  value: number | string
-}
-
-export type Metadata = {
-  name: string
-  description: string
-  image: string
-  animation_url?: string
-  attributes?: Attribute[]
-  external_url?: string
-  type: string
-}
