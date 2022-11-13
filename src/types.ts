1bed3648e3c29072bee996f7ad5ce0dfee9c6f41
Michael Absolon <absolon.mm@gmail.com>
2022-11-13 21:37:54 +0100

    Add types for nodes

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,15 @@
 //  derrived from https://github.com/kodadot/packages/blob/main/minimark/src/common/types.ts
 
 import { SubmittableExtrinsic } from '@polkadot/api/types'
+import { nodeNames } from './maps/consts'
 
 export type UpdateFunction = (name: string, index: number) => string
 export type Extrinsic = SubmittableExtrinsic<'promise'>
 export type ExtrinsicFunction<T> = (arg: T) => Extrinsic
 export type TPalletType = 'xTokens' | 'polkadotXCM' | 'ormlXTokens'
+export type TRelayChainType = 'polkadot' | 'kusama'
+export type TNodeDetails = {
+    name: string,
+    type: TRelayChainType
+}
+export type TNode = typeof nodeNames[number];
