da3a05d907e9f036f9e2820ba9ee2344c87486ae
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2024-01-26 10:50:18 +0100

    fix: PolkadotAssetHub
    Para to Para transfers were sending Teleport instead of Reserve transfer

--- src/nodes/supported/AssetHubPolkadot.ts
+++ src/nodes/supported/AssetHubPolkadot.ts
@@ -22,9 +22,10 @@ class AssetHubPolkadot extends ParachainNode implements IPolkadotXCMTransfer {
   _assetCheckEnabled = false
 
   transferPolkadotXCM(input: PolkadotXCMTransferInput): Extrinsic | TSerializedApiCall {
-    // TESTED https://polkadot.subscan.io/xcm_message/polkadot-e4cdf1c59ffbb3d504adbc893d6b7d72665e484d
-    // TESTED https://polkadot.subscan.io/xcm_message/polkadot-c01158ff1a5c5a596138ed9d0f0f2bccc1d9c51d
-    return PolkadotXCMTransferImpl.transferPolkadotXCM(input, 'limitedTeleportAssets', 'Unlimited')
+    const { scenario } = input
+    const method =
+      scenario === 'ParaToPara' ? 'limitedReserveTransferAssets' : 'limitedTeleportAssets'
+    return PolkadotXCMTransferImpl.transferPolkadotXCM(input, method, 'Unlimited')
   }
 
   transferRelayToPara(options: TTransferRelayToParaOptions): TSerializedApiCall {
