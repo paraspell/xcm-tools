db496c9485faa10a66548f2a5246978dfdfa9b86
Rikkard29 <absolon.mm@gmail.com>
2024-03-06 14:31:41 +0100

    fix(xcm-api): Update redirect URL to landing page

--- apps/xcm-api/src/app.controller.ts
+++ apps/xcm-api/src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Redirect } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  @Redirect('https://github.com/paraspell/xcm-api', 301)
+  @Redirect('https://lightspell.xyz/', 301)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   root() {}
 
