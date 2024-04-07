ffb57658e73e2b568d015ae85b38c73250f4f472
Rikkard29 <absolon.mm@gmail.com>
2024-04-07 02:49:05 +0200

    feature(xcm-analyser): Add XCM Analyser package to monorepo

---
+++ packages/xcm-analyser/scripts/example.ts
@@ -0,0 +1,43 @@
+import { MultiLocation } from '../src/types';
+import { convertMultilocationToUrl, convertXCMToUrls } from '../src/converter/convert';
+
+const multilocation: MultiLocation = {
+  parents: '1',
+  interior: {
+    X2: [{ PalletInstance: '50' }, { GeneralIndex: '1984' }],
+  },
+};
+
+const url = convertMultilocationToUrl(multilocation);
+
+console.log();
+console.log(`URL: ${url}`);
+
+// ParaToPara - Basilisk -> BifrostKusama xTokens.transfer arguments
+const xcmCallArguments = [
+  '1', // currency_id for KSM
+  '100000000000', // amount - 0.1 KSM
+  {
+    // dest
+    V3: {
+      parents: '1',
+      interior: {
+        X2: [
+          {
+            Parachain: '2001', // BifrostKusama paraId
+          },
+          {
+            AccountId32: {
+              network: null,
+              id: '0x84fc49ce30071ea611731838cc7736113c1ec68fbc47119be8a0805066df9b2b',
+            },
+          },
+        ],
+      },
+    },
+  },
+  'Unlimited', // dest_weight_limit
+];
+
+const urls = convertXCMToUrls(xcmCallArguments);
+console.log(`URLs: ${urls}`);
---
+++ packages/xcm-analyser/src/types.ts
@@ -0,0 +1,133 @@
+import { z } from 'zod';
+
+export type JunctionType =
+  | 'Parachain'
+  | 'AccountId32'
+  | 'AccountIndex64'
+  | 'AccountKey20'
+  | 'PalletInstance'
+  | 'GeneralIndex'
+  | 'GeneralKey'
+  | 'OnlyChild'
+  | 'Plurality'
+  | 'GlobalConsensus';
+
+const NetworkId = z.string().nullable();
+const BodyId = z.string().nullable();
+const BodyPart = z.string().nullable();
+const StringOrNumber = z.string().regex(/^\d+$/).or(z.number()).or(z.bigint());
+const HexString = z.string();
+
+const JunctionParachain = z.object({ Parachain: StringOrNumber });
+const JunctionAccountId32 = z.object({
+  AccountId32: z.object({ network: NetworkId, id: HexString }),
+});
+const JunctionAccountIndex64 = z.object({
+  AccountIndex64: z.object({ network: NetworkId, index: StringOrNumber }),
+});
+const JunctionAccountKey20 = z.object({
+  AccountKey20: z.object({ network: NetworkId, key: HexString }),
+});
+const JunctionPalletInstance = z.object({ PalletInstance: StringOrNumber });
+const JunctionGeneralIndex = z.object({ GeneralIndex: StringOrNumber });
+const JunctionGeneralKey = z.object({
+  GeneralKey: z.object({ length: StringOrNumber, data: HexString }),
+});
+const JunctionOnlyChild = z.object({ OnlyChild: z.string() });
+const JunctionPlurality = z.object({
+  Plurality: z.object({ id: BodyId, part: BodyPart }),
+});
+const JunctionGlobalConsensus = z.object({ GlobalConsensus: NetworkId });
+
+const JunctionSchema = z.union([
+  JunctionParachain,
+  JunctionAccountId32,
+  JunctionAccountIndex64,
+  JunctionAccountKey20,
+  JunctionPalletInstance,
+  JunctionGeneralIndex,
+  JunctionGeneralKey,
+  JunctionOnlyChild,
+  JunctionPlurality,
+  JunctionGlobalConsensus,
+]);
+
+const Junctions = z.lazy(() =>
+  z.object({
+    X1: JunctionSchema.optional(),
+    X2: z.tuple([JunctionSchema, JunctionSchema]).optional(),
+    X3: z.tuple([JunctionSchema, JunctionSchema, JunctionSchema]).optional(),
+    X4: z.tuple([JunctionSchema, JunctionSchema, JunctionSchema, JunctionSchema]).optional(),
+    X5: z
+      .tuple([JunctionSchema, JunctionSchema, JunctionSchema, JunctionSchema, JunctionSchema])
+      .optional(),
+    X6: z
+      .tuple([
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+      ])
+      .optional(),
+    X7: z
+      .tuple([
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+      ])
+      .optional(),
+    X8: z
+      .tuple([
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+      ])
+      .optional(),
+    X9: z
+      .tuple([
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+      ])
+      .optional(),
+    X10: z
+      .tuple([
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+        JunctionSchema,
+      ])
+      .optional(),
+  }),
+);
+
+export const MultiLocationSchema = z.object({
+  parents: StringOrNumber,
+  interior: Junctions,
+});
+
+export type MultiLocation = z.infer<typeof MultiLocationSchema>;
+export type Junction = z.infer<typeof JunctionSchema>;
