0cd760fab911d4b5b74891c6ba6c4303d9ba9080
Rikkard29 <absolon.mm@gmail.com>
2024-06-23 02:40:01 +0200

    fix: Fix WS endpoints timing out

--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -195,7 +195,7 @@ export const createApiInstance = async (wsUrl: string): Promise<ApiPromise> => {
 export const createApiInstanceForNode = async (node: TNodeWithRelayChains): Promise<ApiPromise> => {
   if (node === 'Polkadot' || node === 'Kusama') {
     const endpointOption = node === 'Polkadot' ? prodRelayPolkadot : prodRelayKusama
-    const wsUrl = Object.values(endpointOption.providers)[1]
+    const wsUrl = Object.values(endpointOption.providers)[0]
     return await createApiInstance(wsUrl)
   }
   return await getNode(node).createApiInstance()
