dcf937da7276973b3ec33783a76bf80f471d263d
Rikkard29 <absolon.mm@gmail.com>
2024-06-23 20:49:33 +0200

    feat: Add support for multilocations in XCM API

--- apps/xcm-api/src/x-transfer/x-transfer.controller.ts
+++ apps/xcm-api/src/x-transfer/x-transfer.controller.ts
@@ -1,24 +1,47 @@
-import { Controller, Get, Query, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  UsePipes,
+} from '@nestjs/common';
 import { XTransferService } from './x-transfer.service.js';
-import { XTransferDto } from './dto/XTransferDto.js';
 import { AnalyticsService } from '../analytics/analytics.service.js';
 import { EventName } from '../analytics/EventName.js';
+import { ZodValidationPipe } from '../zod-validation-pipe.js';
+import { XTransferDto, XTransferDtoSchema } from './dto/XTransferDto.js';
 
 @Controller('x-transfer')
+@UsePipes(new ZodValidationPipe(XTransferDtoSchema))
 export class XTransferController {
   constructor(
     private xTransferService: XTransferService,
     private analyticsService: AnalyticsService,
   ) {}
 
+  private trackAnalytics(eventName: EventName, req, params: XTransferDto) {
+    const { from, to, currency } = params;
+    const resolvedCurrency =
+      typeof currency === 'string' ? currency : 'MultiLocation';
+    const resolvedTo = typeof to === 'string' ? to : 'MultiLocation';
+    this.analyticsService.track(eventName, req, {
+      from,
+      resolvedTo,
+      resolvedCurrency,
+    });
+  }
+
   @Get()
   generateXcmCall(@Query() queryParams: XTransferDto, @Req() req) {
-    const { from, to, currency } = queryParams;
-    this.analyticsService.track(EventName.GENERATE_XCM_CALL, req, {
-      from,
-      to,
-      currency,
-    });
+    this.trackAnalytics(EventName.GENERATE_XCM_CALL, req, queryParams);
     return this.xTransferService.generateXcmCall(queryParams);
+  }
+
+  @Post()
+  generateXcmCallV2(@Body() bodyParams: XTransferDto, @Req() req) {
+    this.trackAnalytics(EventName.GENERATE_XCM_CALL, req, bodyParams);
+    return this.xTransferService.generateXcmCall(bodyParams);
   }
 }
---
+++ apps/xcm-api/src/zod-validation-pipe.ts
@@ -0,0 +1,19 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { ZodSchema } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      console.log('error', error);
+
+      throw new BadRequestException({
+        message: error.errors,
+      });
+    }
+  }
+}
