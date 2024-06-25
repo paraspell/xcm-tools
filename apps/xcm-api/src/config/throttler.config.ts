92e3d4adbb95a4ac633b915c4b47c4f4a0834f58
Rikkard29 <absolon.mm@gmail.com>
2024-06-25 02:24:27 +0200

    chore(xcm-api): Update pnpm dependencies

--- apps/xcm-api/src/config/throttler.config.ts
+++ apps/xcm-api/src/config/throttler.config.ts
@@ -4,14 +4,19 @@ import { ConfigService } from '@nestjs/config';
 export const throttlerConfig = async (
   config: ConfigService,
 ): Promise<ThrottlerModuleOptions> => ({
-  ttl: process.env.NODE_ENV === 'test' ? 0 : config.get('RATE_LIMIT_TTL_SEC'),
-  limit: (context) => {
-    const request = context.switchToHttp().getRequest();
-    if (request.user && request.user.requestLimit) {
-      return request.user.requestLimit;
-    }
-    return request.user
-      ? config.get('RATE_LIMIT_REQ_COUNT_AUTH')
-      : config.get('RATE_LIMIT_REQ_COUNT_PUBLIC');
-  },
+  throttlers: [
+    {
+      ttl:
+        process.env.NODE_ENV === 'test' ? 0 : config.get('RATE_LIMIT_TTL_SEC'),
+      limit: (context) => {
+        const request = context.switchToHttp().getRequest();
+        if (request.user && request.user.requestLimit) {
+          return request.user.requestLimit;
+        }
+        return request.user
+          ? config.get('RATE_LIMIT_REQ_COUNT_AUTH')
+          : config.get('RATE_LIMIT_REQ_COUNT_PUBLIC');
+      },
+    },
+  ],
 });
