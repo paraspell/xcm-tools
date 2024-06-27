3d6c68b9b146aa0d42210e84b0f332c3bf60c0aa
Rikkard29 <absolon.mm@gmail.com>
2024-06-27 15:27:00 +0200

    feat: Add support for V4 MultiAsset

--- packages/sdk/src/types/TMultiAsset.ts
+++ packages/sdk/src/types/TMultiAsset.ts
@@ -1,8 +1,17 @@
 import { type TMultiLocation } from './TMultiLocation'
 import { type TAmount } from './TTransfer'
 
-export interface TMultiAsset {
+export type TMultiAsset = TMultiAssetV3 | TMultiAssetV4
+
+export interface TMultiAssetV3 {
   id: { Concrete: TMultiLocation }
+  fun: {
+    Fungible: TAmount
+  }
+}
+
+export interface TMultiAssetV4 {
+  id: TMultiLocation
   fun: {
     Fungible: TAmount
   }
