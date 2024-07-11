b75b4e513be56be4aaece8d237f87cb1e39875cc
Michael Absolon <absolon.mm@gmail.com>
2024-07-12 00:07:26 +0200

    feat(visualizator-fe): Update @polkadot/apps-config & modify logos

--- apps/visualizator-fe/src/components/Parachain/utils.ts
+++ apps/visualizator-fe/src/components/Parachain/utils.ts
@@ -21,6 +21,18 @@ import crustPng from '../../logos/crust1.png';
 import composableFinancePng from '../../logos/composableFinance1.png';
 import nodlePng from '../../logos/nodle1.png';
 import darwiniaPng from '../../logos/darwinia1.png';
+import continuumPng from '../../logos/continuum.png';
+import aventusPng from '../../logos/aventus.png';
+import ajunaPng from '../../logos/ajuna.png';
+import frequencyPng from '../../logos/frequency.png';
+import hyperbridgePng from '../../logos/hyperbridge.png';
+import hashedPng from '../../logos/hashed.png';
+import laosPng from '../../logos/laos.png';
+import mythosPng from '../../logos/mythos.png';
+import polimecPng from '../../logos/polimec.png';
+import t3rnPng from '../../logos/t3rn.png';
+import soraPng from '../../logos/sora.png';
+import logionPng from '../../logos/logion.png';
 import { getParachainLogo } from '../../utils/utils';
 
 export const getNodeLogo = (node: string) => {
@@ -47,6 +59,18 @@ export const getNodeLogo = (node: string) => {
   if (node === 'Composable Finance') return composableFinancePng;
   if (node === 'Nodle') return nodlePng;
   if (node === 'Darwinia') return darwiniaPng;
+  if (node === 'Continuum') return continuumPng;
+  if (node === 'Aventus') return aventusPng;
+  if (node === 'Ajuna Network') return ajunaPng;
+  if (node === 'Frequency') return frequencyPng;
+  if (node === 'Hyperbridge (Nexus)') return hyperbridgePng;
+  if (node === 'Hashed Network') return hashedPng;
+  if (node === 'Laos') return laosPng;
+  if (node === 'Mythos') return mythosPng;
+  if (node === 'Polimec') return polimecPng;
+  if (node === 't3rn') return t3rnPng;
+  if (node === 'SORA') return soraPng;
+  if (node === 'Logion') return logionPng;
   return getParachainLogo(node);
 };
 
@@ -68,5 +92,17 @@ export const getLogoScaleFactor = (node: string) => {
   if (node === 'Composable Finance') return 1.75;
   if (node === 'Nodle') return 1.5;
   if (node === 'Manta') return 2;
+  if (node === 'Continuum') return 1.75;
+  if (node === 'Aventus') return 1.75;
+  if (node === 'Ajuna Network') return 1.75;
+  if (node === 'Frequency') return 1.75;
+  if (node === 'Hyperbridge (Nexus)') return 1.4;
+  if (node === 'Hashed Network') return 2;
+  if (node === 'Laos') return 2;
+  if (node === 'Mythos') return 1.5;
+  if (node === 'Polimec') return 1.5;
+  if (node === 't3rn') return 2;
+  if (node === 'SORA') return 2;
+  if (node === 'Logion') return 2;
   return 3;
 };
