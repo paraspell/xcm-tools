220da1b6d060b7aa4d8262e779256e40ce145f3f
Michael Absolon <absolon.mm@gmail.com>
2024-08-05 02:54:52 +0200

    ci: Integrate Codecov PR comments 💬

--- packages/xcm-router/vitest.config.ts
+++ packages/xcm-router/vitest.config.ts
@@ -2,7 +2,7 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, 'playground/**/*', '**/*.integration.test.ts'],
+    exclude: [...configDefaults.exclude, '**/*.integration.test.ts'],
     testTimeout: 120000,
     hookTimeout: 120000,
   },
