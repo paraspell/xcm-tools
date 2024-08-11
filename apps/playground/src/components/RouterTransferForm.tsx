b55644e12d98e1931329518d1a200a763ba569e7
Michael Absolon <absolon.mm@gmail.com>
2024-08-12 01:15:32 +0200

    feat(xcm-router): Add Snowbridge transfers support ❄️

--- apps/playground/src/components/RouterTransferForm.tsx
+++ apps/playground/src/components/RouterTransferForm.tsx
@@ -7,12 +7,16 @@ import {
 import { isValidWalletAddress } from "../utils";
 import { FC, useEffect, useState } from "react";
 import {
+  Text,
   Button,
   Checkbox,
   Group,
   Select,
   Stack,
   TextInput,
+  Tooltip,
+  Center,
+  rem,
 } from "@mantine/core";
 import {
   getAllAssetsSymbols,
@@ -24,7 +28,9 @@ import { web3Accounts, web3FromAddress } from "@polkadot/extension-dapp";
 import AccountsModal from "./AccountsModal";
 import { useDisclosure } from "@mantine/hooks";
 import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
-import { ethers } from "ethers";
+import { BrowserProvider, ethers } from "ethers";
+import { IconInfoCircle } from "@tabler/icons-react";
+import EthAccountsModal from "./EthAccountsModal";
 
 export type TAutoSelect = "Auto select";
 
@@ -41,6 +47,7 @@ export type FormValues = {
   useApi: boolean;
   evmSigner?: Signer;
   evmInjectorAddress?: string;
+  ethSigner?: ethers.Signer;
 };
 
 type Props = {
@@ -52,13 +59,48 @@ const RouterTransferForm: FC<Props> = ({ onSubmit, loading }) => {
   const [modalOpened, { open: openModal, close: closeModal }] =
     useDisclosure(false);
 
+  const [
+    assetHubModalOpened,
+    { open: openAssetHubModal, close: closeAssetHubModal },
+  ] = useDisclosure(false);
+
+  const [ethModalOpened, { open: openEthModal, close: closeEthModal }] =
+    useDisclosure(false);
+
   const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
+  const [ethAccounts, setEthAccounts] = useState<string[]>([]);
+  const [assetHubAccounts, setAssetHubAccounts] = useState<
+    InjectedAccountWithMeta[]
+  >([]);
   const [selectedAccount, setSelectedAccount] =
     useState<InjectedAccountWithMeta>();
+
+  const [selectedAssetHubAccount, setSelectedAssetHubAccount] =
+    useState<InjectedAccountWithMeta>();
+
+  const [provider, setProvider] = useState<BrowserProvider | null>(null);
+  const [selectedEthAccount, setSelectedEthAccount] = useState<string | null>(
+    null
+  );
 
   const onAccountSelect = (account: InjectedAccountWithMeta) => () => {
     setSelectedAccount(account);
     closeModal();
+  };
+
+  const onEthAccountSelect = (account: string) => async () => {
+    setSelectedEthAccount(account);
+    if (!provider) {
+      throw new Error("Provider not initialized");
+    }
+    const tempSigner = await provider.getSigner(account);
+    form.setFieldValue("ethSigner", tempSigner);
+    closeEthModal();
+  };
+
+  const onAssetHubAccountSelect = (account: InjectedAccountWithMeta) => () => {
+    setSelectedAssetHubAccount(account);
+    closeAssetHubModal();
   };
 
   useEffect(() => {
@@ -70,6 +112,29 @@ const RouterTransferForm: FC<Props> = ({ onSubmit, loading }) => {
       }
     })();
   }, [selectedAccount]);
+
+  useEffect(() => {
+    void (async () => {
+      if (selectedEthAccount) {
+        if (!provider) {
+          throw new Error("Provider not initialized");
+        }
+
+        const signer = await provider.getSigner();
+
+        if (!signer) {
+          throw new Error("Signer not initialized");
+        }
+        form.setFieldValue("ethSigner", signer);
+      }
+    })();
+  }, [selectedEthAccount]);
+
+  useEffect(() => {
+    if (selectedAssetHubAccount) {
+      form.setFieldValue("assetHubAddress", selectedAssetHubAccount.address);
+    }
+  }, [selectedAssetHubAccount]);
 
   const form = useForm<FormValues>({
     initialValues: {
@@ -91,6 +156,34 @@ const RouterTransferForm: FC<Props> = ({ onSubmit, loading }) => {
     },
   });
 
+  useEffect(() => {
+    if (form.values.from === "Ethereum" || form.values.to === "Ethereum") {
+      onAccountDisconnect();
+    }
+  }, [form.values.from, form.values.to]);
+
+  useEffect(() => {
+    if (form.values.from !== "Ethereum" || form.values.to !== "Ethereum") {
+      onAssetHubAccountDisconnect();
+      onEthWalletDisconnect();
+    }
+  }, [form.values.from, form.values.to]);
+
+  const connectAssetHubWallet = async () => {
+    try {
+      const allAccounts = await web3Accounts();
+      setAssetHubAccounts(
+        allAccounts.filter((account) => !ethers.isAddress(account.address))
+      );
+      openAssetHubModal();
+    } catch (error) {
+      console.error("Failed to connect EVM wallet:", error);
+      alert("Failed to connect EVM wallet");
+    }
+  };
+
+  const onConnectAssetHubWallet = () => void connectAssetHubWallet();
+
   const connectEvmWallet = async () => {
     try {
       const allAccounts = await web3Accounts();
@@ -106,6 +199,29 @@ const RouterTransferForm: FC<Props> = ({ onSubmit, loading }) => {
 
   const onConnectEvmWallet = () => void connectEvmWallet();
 
+  const connectEthWallet = async () => {
+    if (!window.ethereum) {
+      alert("Please install MetaMask!");
+      return;
+    }
+
+    const tempProvider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(tempProvider);
+    try {
+      const accounts = (await tempProvider.send(
+        "eth_requestAccounts",
+        []
+      )) as string[];
+      console.log("Accounts:", accounts);
+      setEthAccounts(accounts);
+      openEthModal();
+    } catch (error) {
+      console.error("Error connecting to MetaMask:", error);
+    }
+  };
+
+  const onConnectEthWallet = () => void connectEthWallet();
+
   const onAccountDisconnect = () => {
     setSelectedAccount(undefined);
     form.setFieldValue("evmSigner", undefined);
@@ -113,14 +229,87 @@ const RouterTransferForm: FC<Props> = ({ onSubmit, loading }) => {
     closeModal();
   };
 
+  const onAssetHubAccountDisconnect = () => {
+    setSelectedAssetHubAccount(undefined);
+    form.setFieldValue("assetHubAddress", undefined);
+    closeAssetHubModal();
+  };
+
+  const onEthWalletDisconnect = () => {
+    setSelectedEthAccount(null);
+    form.setFieldValue("ethSigner", undefined);
+    closeEthModal();
+  };
+
   const fromCurrencySymbols = [
     ...new Set(getAllAssetsSymbols(form.values.from)),
   ];
   const toCurrencySymbols = [...new Set(getAllAssetsSymbols(form.values.to))];
 
+  const infoEthWallet = (
+    <Tooltip
+      label="You need to connect your Ethereum wallet when choosing Ethereum as the origin or destination node"
+      position="top-end"
+      withArrow
+      transitionProps={{ transition: "pop-bottom-right" }}
+    >
+      <Text component="div" style={{ cursor: "help" }}>
+        <Center>
+          <IconInfoCircle
+            style={{ width: rem(18), height: rem(18) }}
+            stroke={1.5}
+          />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
+
+  const infoAssetHubWallet = (
+    <Tooltip
+      label="You need to connect your AssetHub wallet when choosing Ethereum as the origin or destination node"
+      position="top-end"
+      withArrow
+      transitionProps={{ transition: "pop-bottom-right" }}
+    >
+      <Text component="div" style={{ cursor: "help" }}>
+        <Center>
+          <IconInfoCircle
+            style={{ width: rem(18), height: rem(18) }}
+            stroke={1.5}
+          />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
+
+  const infoEvmWallet = (
+    <Tooltip
+      label="You need to connect yout Polkadot EVM wallet when choosing EVM node as origin"
+      position="top-end"
+      withArrow
+      transitionProps={{ transition: "pop-bottom-right" }}
+    >
+      <Text component="div" style={{ cursor: "help" }}>
+        <Center>
+          <IconInfoCircle
+            style={{ width: rem(18), height: rem(18) }}
+            stroke={1.5}
+          />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
+
   return (
     <form onSubmit={form.onSubmit(onSubmit)}>
       <Stack>
+        <EthAccountsModal
+          isOpen={ethModalOpened}
+          onClose={closeEthModal}
+          accounts={ethAccounts}
+          onAccountSelect={onEthAccountSelect}
+          onDisconnect={onAccountDisconnect}
+        />
         <AccountsModal
           isOpen={modalOpened}
           onClose={closeModal}
@@ -128,6 +317,14 @@ const RouterTransferForm: FC<Props> = ({ onSubmit, loading }) => {
           onAccountSelect={onAccountSelect}
           title="Select evm account"
           onDisconnect={onAccountDisconnect}
+        />
+        <AccountsModal
+          isOpen={assetHubModalOpened}
+          onClose={closeAssetHubModal}
+          accounts={assetHubAccounts}
+          onAccountSelect={onAssetHubAccountSelect}
+          title="Select AssetHub account"
+          onDisconnect={onAssetHubAccountDisconnect}
         />
         <Select
           label="Origin node"
@@ -202,6 +399,8 @@ const RouterTransferForm: FC<Props> = ({ onSubmit, loading }) => {
               TransactionType.TO_DESTINATION.toString(),
               TransactionType.SWAP.toString(),
               TransactionType.FULL_TRANSFER.toString(),
+              TransactionType.FROM_ETH.toString(),
+              TransactionType.TO_ETH.toString(),
             ]}
             searchable
             required
@@ -218,12 +417,47 @@ const RouterTransferForm: FC<Props> = ({ onSubmit, loading }) => {
 
         <Group justify="space-between">
           <Checkbox label="Use XCM API" {...form.getInputProps("useApi")} />
-
-          <Button size="xs" variant="white" onClick={onConnectEvmWallet}>
-            {selectedAccount
-              ? `${selectedAccount?.meta.name} (${selectedAccount?.meta.source})`
-              : "Connect EVM wallet"}
-          </Button>
+          <Button.Group orientation="vertical">
+            {(form.values.from === "Ethereum" ||
+              form.values.to === "Ethereum") && (
+              <Button
+                size="xs"
+                variant="outline"
+                onClick={onConnectEthWallet}
+                rightSection={infoEthWallet}
+              >
+                {selectedEthAccount
+                  ? `Connected: ${selectedEthAccount.substring(0, 6)}...${selectedEthAccount.substring(selectedEthAccount.length - 4)}`
+                  : "Connect Ethereum Wallet"}
+              </Button>
+            )}
+            {(form.values.from === "Ethereum" ||
+              form.values.to === "Ethereum") && (
+              <Button
+                size="xs"
+                variant="outline"
+                onClick={onConnectAssetHubWallet}
+                rightSection={infoAssetHubWallet}
+              >
+                {selectedAssetHubAccount
+                  ? `${selectedAssetHubAccount?.meta.name} (${selectedAssetHubAccount?.meta.source})`
+                  : "Connect AssetHub wallet"}
+              </Button>
+            )}
+            {form.values.from !== "Ethereum" &&
+              form.values.to !== "Ethereum" && (
+                <Button
+                  size="xs"
+                  variant="outline"
+                  onClick={onConnectEvmWallet}
+                  rightSection={infoEvmWallet}
+                >
+                  {selectedAccount
+                    ? `${selectedAccount?.meta.name} (${selectedAccount?.meta.source})`
+                    : "Connect EVM wallet"}
+                </Button>
+              )}
+          </Button.Group>
         </Group>
 
         <Button type="submit" loading={loading}>
