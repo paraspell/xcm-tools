be655457f7673e5c5d47cc7ccb39278fe7463989
Michael Absolon <absolon.mm@gmail.com>
2024-09-21 10:47:41 +0200

    build(sdk): Setup more benevolent peer dependencies 📦

---
+++ packages/sdk/src/nodes/supported/CoretimePolkadot.test.ts
@@ -0,0 +1,102 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import {
+  Extrinsic,
+  PolkadotXCMTransferInput,
+  TRelayToParaInternalOptions,
+  Version
+} from '../../types'
+import { getNode } from '../../utils'
+import PolkadotXCMTransferImpl from '../polkadotXcm'
+import CoretimePolkadot from './CoretimePolkadot'
+import { constructRelayToParaParameters } from '../../pallets/xcmPallet/utils'
+
+vi.mock('../../pallets/xcmPallet/utils', async () => {
+  const actual = await vi.importActual<typeof import('../../pallets/xcmPallet/utils')>(
+    '../../pallets/xcmPallet/utils'
+  )
+  return {
+    ...actual,
+    constructRelayToParaParameters: vi.fn().mockReturnValue('mocked_parameters')
+  }
+})
+
+vi.mock('../polkadotXcm', async () => {
+  const actual = await vi.importActual<typeof import('../polkadotXcm')>('../polkadotXcm')
+  return {
+    default: {
+      ...actual.default,
+      transferPolkadotXCM: vi.fn()
+    }
+  }
+})
+
+describe('CoretimePolkadot', () => {
+  let coretimePolkadot: CoretimePolkadot
+
+  beforeEach(() => {
+    coretimePolkadot = getNode('CoretimePolkadot')
+  })
+
+  it('should be instantiated correctly', () => {
+    expect(coretimePolkadot).toBeInstanceOf(CoretimePolkadot)
+  })
+
+  describe('transferPolkadotXCM', () => {
+    it('should use limitedReserveTransferAssets when scenario is ParaToPara', () => {
+      const input = { scenario: 'ParaToPara' } as PolkadotXCMTransferInput
+
+      const mockResult = {} as Extrinsic
+
+      vi.mocked(PolkadotXCMTransferImpl.transferPolkadotXCM).mockResolvedValue(mockResult)
+
+      coretimePolkadot.transferPolkadotXCM(input)
+
+      expect(PolkadotXCMTransferImpl.transferPolkadotXCM).toHaveBeenCalledWith(
+        input,
+        'limitedReserveTransferAssets',
+        'Unlimited'
+      )
+    })
+
+    it('should use limitedTeleportAssets when scenario is not ParaToPara', () => {
+      const input = { scenario: 'ParaToRelay' } as PolkadotXCMTransferInput
+      coretimePolkadot.transferPolkadotXCM(input)
+      expect(PolkadotXCMTransferImpl.transferPolkadotXCM).toHaveBeenCalledWith(
+        input,
+        'limitedTeleportAssets',
+        'Unlimited'
+      )
+    })
+  })
+
+  describe('transferRelayToPara', () => {
+    it('should return correct serialized API call with default version', () => {
+      const options = {} as TRelayToParaInternalOptions
+      const result = coretimePolkadot.transferRelayToPara(options)
+      expect(result).toEqual({
+        module: 'xcmPallet',
+        section: 'limitedTeleportAssets',
+        parameters: 'mocked_parameters'
+      })
+      expect(constructRelayToParaParameters).toHaveBeenCalledWith(options, Version.V3, true)
+    })
+    it('should return correct serialized API call with specified version', () => {
+      const options = { version: Version.V2 } as TRelayToParaInternalOptions
+      const result = coretimePolkadot.transferRelayToPara(options)
+      expect(result).toEqual({
+        module: 'xcmPallet',
+        section: 'limitedTeleportAssets',
+        parameters: 'mocked_parameters'
+      })
+      expect(constructRelayToParaParameters).toHaveBeenCalledWith(options, Version.V2, true)
+    })
+
+    describe('getProvider', () => {
+      it('should return the correct provider URL', () => {
+        const result = coretimePolkadot.getProvider()
+        expect(result).toBe('wss://polkadot-coretime-rpc.polkadot.io')
+      })
+    })
+  })
+})
