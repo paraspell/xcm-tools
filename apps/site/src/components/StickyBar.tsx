de7c1cae8a9c1bd1905f7da95eb76d4cf8be7605
Michael Absolon <absolon.mm@gmail.com>
2024-10-01 18:46:17 +0200

    feat(site): Add compliance statement 🧑‍⚖️

--- apps/site/src/components/StickyBar.tsx
+++ apps/site/src/components/StickyBar.tsx
@@ -1,7 +1,15 @@
-import { ActionIcon, Flex, Text } from "@mantine/core";
+import { ActionIcon, Flex, Group, Text, Tooltip } from "@mantine/core";
 import AudioPlayer from "./AudioPlayer";
 import { FC } from "react";
-import { IconX } from "@tabler/icons-react";
+import { IconInfoCircle, IconX } from "@tabler/icons-react";
+
+const complianceStatement = `
+Compliance statement:
+
+This podcast was generated using NotebookLM, a free tool provided by Google. The podcast is for informational purposes only, and the views and opinions expressed are those of the author and do not necessarily reflect the views of Google.
+
+By using this podcast, you acknowledge that you have read and agree to Google's terms of service and privacy policy.
+`;
 
 type Props = {
   onCloseClick: () => void;
@@ -16,15 +24,28 @@ const StickyBar: FC<Props> = ({ onCloseClick }) => (
     direction={{ base: "column", sm: "row" }}
     pos="relative"
   >
-    <Text
-      size="sm"
-      fw={600}
-      c="white"
-      mr={{ base: 0, sm: "md" }}
+    <Group
+      gap={4}
+      mr={{ base: 0, sm: "xl" }}
       mb={{ base: 3, sm: 0 }}
+      align="center"
     >
-      Listen to AI generated podcast about ParaSpell XCM Tools
-    </Text>
+      <Text size="sm" fw={600} c="white">
+        Listen to AI generated podcast about ParaSpell XCM Tools
+      </Text>
+      <Tooltip
+        label={complianceStatement}
+        position="bottom"
+        withArrow
+        multiline
+        maw={520}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <IconInfoCircle color="white" size={16} />
+        </div>
+      </Tooltip>
+    </Group>
+
     <AudioPlayer src="https://github.com/paraspell/presskit/blob/main/podcasts_notebooklm/ParaSpell_Podcast_by_NotebookLM.wav?raw=true" />
     <ActionIcon
       size="xs"
