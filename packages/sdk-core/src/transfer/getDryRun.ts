44c8a105e1d5195dca6beab4c612d835f3116710
Michael Absolon <absolon.mm@gmail.com>
2025-03-04 18:15:52 +0100

    fix(sdk-core): Validate sender address in dry-run function 🛠️

--- packages/sdk-core/src/transfer/getDryRun.ts
+++ packages/sdk-core/src/transfer/getDryRun.ts
@@ -1,9 +1,12 @@
 import type { TDryRunOptions, TDryRunResult } from '../types'
+import { validateAddress } from '../utils'
 
 export const getDryRun = async <TApi, TRes>(
   options: TDryRunOptions<TApi, TRes>
 ): Promise<TDryRunResult> => {
-  const { api, node } = options
+  const { api, node, address } = options
+
+  validateAddress(address, node, false)
 
   await api.init(node)
   try {
