eee00395252e001378ec3b5936f72c33ce4b1d34
Michael Absolon <absolon.mm@gmail.com>
2025-05-07 11:01:31 +0200

    fix: Add asset claim address validation 🪄

--- packages/sdk-core/src/pallets/assets/asset-claim/assetClaim.ts
+++ packages/sdk-core/src/pallets/assets/asset-claim/assetClaim.ts
@@ -3,12 +3,15 @@ import { isRelayChain } from '@paraspell/sdk-common'
 
 import { TX_CLIENT_TIMEOUT_MS } from '../../../constants'
 import { type TAssetClaimOptions } from '../../../types/TAssetClaim'
+import { validateAddress } from '../../../utils'
 import { buildClaimAssetsInput } from './buildClaimAssetsInput'
 
 export const claimAssets = async <TApi, TRes>(
   options: TAssetClaimOptions<TApi, TRes>
 ): Promise<TRes> => {
-  const { api, node } = options
+  const { api, node, address } = options
+
+  validateAddress(address, node)
 
   await api.init(node, TX_CLIENT_TIMEOUT_MS)
 
