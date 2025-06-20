14f42d344ed0a637747858f93543c26fd111bad3
Michael Absolon <absolon.mm@gmail.com>
2025-06-20 16:49:12 +0200

    build(xcm-api): Add Sentry DSN to .env 🛠️

--- apps/xcm-api/src/main.ts
+++ apps/xcm-api/src/main.ts
@@ -1,4 +1,6 @@
-import './instrument.js';
+// eslint-disable-next-line simple-import-sort/exports
+import { init as sentryInit } from '@sentry/nestjs';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -6,13 +8,30 @@ import type { ExpressAdapter } from '@nestjs/platform-express';
 import { replaceBigInt } from '@paraspell/sdk';
 
 import { AppModule } from './app.module.js';
+import { ConfigService } from '@nestjs/config';
 
 export const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const dsn = configService.get<string>('SENTRY_DSN');
+
+  if (dsn) {
+    sentryInit({
+      dsn: configService.get<string>('SENTRY_DSN'),
+      integrations: [nodeProfilingIntegration()],
+      tracesSampleRate: 1.0,
+      profilesSampleRate: 1.0,
+      environment: configService.get<string>('NODE_ENV') || 'development',
+    });
+  }
+
   (app.getHttpAdapter().getInstance() as ExpressAdapter).set(
     'json replacer',
     replaceBigInt,
   );
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const DEFAULT_PORT = 3001;
