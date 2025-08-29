b0383b4eccf83708fc167fecc2ed95acc3f40eb4
Salgac <domco.salgovic@gmail.com>
2025-08-29 15:25:22 +0200

    feat: Add Resources tab to footer

--- apps/visualizer-fe/src/components/Footer/Footer.tsx
+++ apps/visualizer-fe/src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Group, Paper } from '@mantine/core';
+import { ActionIcon, Box, Button, Flex, Group, Input, Paper } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { IconBrandGithubFilled, IconNotes } from '@tabler/icons-react';
 import { useTranslation } from 'react-i18next';
 
 import { useSelectedParachain } from '../../context/SelectedParachain/useSelectedParachain';
@@ -14,14 +15,40 @@ const Footer = () => {
 
   return (
     <Box pos="absolute" bottom={0} left={0} p="xl" w="100%">
-      <Paper w="100%" p="md" pt="sm" pb="sm" radius="md" bg="rgba(255,255,255,0.8)">
-        <Group align="flex-end">
-          <ParachainSelector value={parachains} onCustomChange={setParachains} flex={1} />
-          <DateRangePicker value={dateRange} setValue={setDateRange} />
-          <OptionsModal opened={opened} close={close} />
-          <Button onClick={open}>{t('options.title')}</Button>
-        </Group>
-      </Paper>
+      <Flex gap="md" align="end" w="100%">
+        <Paper p="md" radius="md" bg="rgba(255,255,255,0.8)" flex="0 0 auto">
+          <Input.Label>{t('resources')}</Input.Label>
+          <Group gap="sm" wrap="nowrap">
+            <ActionIcon
+              component="a"
+              href="https://github.com/paraspell/xcm-tools/tree/main/apps/visualizer-fe"
+              target="_blank"
+              color="gray.6"
+              size={36}
+            >
+              <IconBrandGithubFilled size={20} color="white" />
+            </ActionIcon>
+            <ActionIcon
+              component="a"
+              href="https://paraspell.github.io/docs/visualizer/getting-start.html"
+              target="_blank"
+              color="gray.6"
+              size={36}
+            >
+              <IconNotes size={20} color="white" />
+            </ActionIcon>
+          </Group>
+        </Paper>
+
+        <Paper p="md" radius="md" bg="rgba(255,255,255,0.8)" flex={1}>
+          <Group gap="md" align="end" wrap="nowrap">
+            <ParachainSelector value={parachains} onCustomChange={setParachains} flex={1} />
+            <DateRangePicker value={dateRange} setValue={setDateRange} />
+            <OptionsModal opened={opened} close={close} />
+            <Button onClick={open}>{t('options.title')}</Button>
+          </Group>
+        </Paper>
+      </Flex>
     </Box>
   );
 };
