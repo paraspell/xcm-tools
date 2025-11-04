3ff2f93bde17a5e33a7f254d99840a0e2a3bc7a0
Salgac <domco.salgovic@gmail.com>
2025-11-04 16:21:05 +0100

    feat(visualizer-fe): Tweak content panels

--- apps/visualizer-fe/src/components/CollapseButton.tsx
+++ apps/visualizer-fe/src/components/CollapseButton.tsx
@@ -1,34 +1,48 @@
 import { ActionIcon } from '@mantine/core';
+import type { SpringValue } from '@react-spring/web';
+import { animated } from '@react-spring/web';
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
 import type { FC } from 'react';
+
+const AnimatedDiv = animated('div');
 
 type Props = {
   onClick: () => void;
   isCollapsed: boolean;
   isMobile: boolean;
+  slideStyles: { transform: SpringValue<string> };
 };
 
-const CollapseButton: FC<Props> = ({ onClick, isCollapsed, isMobile }) => (
-  <ActionIcon
-    onClick={onClick}
-    variant="default"
-    pos="absolute"
-    left={0}
-    top="50%"
-    h={isCollapsed ? 50 : 100}
-    w={20}
-    mih={1}
-    miw={1}
+const BUTTON_WIDTH = 20;
+
+const CollapseButton: FC<Props> = ({ onClick, isCollapsed, isMobile, slideStyles }) => (
+  <AnimatedDiv
     style={{
-      transform: isCollapsed || !isMobile ? 'translate(-100%, -50%)' : 'translate(0, -50%)',
-      borderTopRightRadius: 0,
-      borderBottomRightRadius: 0,
-      borderWidth: 0,
-      zIndex: 69
+      ...slideStyles,
+      position: 'absolute',
+      top: '50%',
+      left: isMobile && !isCollapsed ? 0 : -BUTTON_WIDTH,
+      zIndex: 1000,
+      transform: slideStyles.transform.to(t => {
+        const compensate = isCollapsed ? `translateX(-${2 * BUTTON_WIDTH}px)` : '';
+        return `${t} ${compensate}`;
+      })
     }}
   >
-    {isCollapsed ? <IconChevronLeft size={24} /> : <IconChevronRight size={24} />}
-  </ActionIcon>
+    <ActionIcon
+      onClick={onClick}
+      variant="default"
+      h={isCollapsed ? 50 : 100}
+      w={BUTTON_WIDTH}
+      style={{
+        borderTopRightRadius: 0,
+        borderBottomRightRadius: 0,
+        borderWidth: 0
+      }}
+    >
+      {isCollapsed ? <IconChevronLeft size={24} /> : <IconChevronRight size={24} />}
+    </ActionIcon>
+  </AnimatedDiv>
 );
 
 export default CollapseButton;
--- apps/visualizer-fe/src/components/RightPanel.tsx
+++ apps/visualizer-fe/src/components/RightPanel.tsx
@@ -12,40 +12,61 @@ const AnimatedDiv = animated('div');
 const RightPanel = () => {
   const { isMobile } = useDeviceType();
 
-  const [width, setWidth] = useState(isMobile ? '0%' : '40%');
-  const props = useSpring({ width });
+  const panelWidth = isMobile ? '100%' : '40%';
+  const [collapsed, setCollapsed] = useState(isMobile);
 
-  const toggleWidth = () => {
-    const maxWidth = isMobile ? '100%' : '40%';
-    setWidth(width === '0%' ? maxWidth : '0%');
-  };
-
-  const isCollapsed = width === '0%';
+  const slideStyles = useSpring({
+    transform: collapsed ? 'translateX(100%)' : 'translateX(0%)',
+    config: {
+      tension: 170,
+      friction: 26
+    }
+  });
 
   return (
-    <AnimatedDiv
+    <div
       style={{
-        ...props,
+        width: collapsed ? 0 : panelWidth,
+        flexShrink: 0,
+        height: '100%',
         position: isMobile ? 'absolute' : 'relative',
         right: 0,
         top: 0,
-        height: '100%',
-        zIndex: isMobile ? 10 : 1
+        zIndex: isMobile ? 10 : 1,
+        overflow: 'visible',
+        display: 'flex'
       }}
     >
-      <Stack h="100%" w="100%" pos="relative" bg="white">
-        <CollapseButton onClick={toggleWidth} isCollapsed={isCollapsed} isMobile={isMobile} />
-        <Flex flex={1} w="100%" h="50%" pl={10} mih={0} justify="center" align="stretch">
-          <TabNavigator defaultValue={PageRoute.SCENE_2D_ASSETS_CHART} />
-        </Flex>
+      <CollapseButton
+        onClick={() => setCollapsed(v => !v)}
+        isCollapsed={collapsed}
+        isMobile={isMobile}
+        slideStyles={slideStyles}
+      />
 
-        {!isMobile && (
-          <Flex flex={1} w="100%" h="50%" pl={10} mih={0} justify="center" align="stretch">
-            <TabNavigator />
+      <AnimatedDiv
+        style={{
+          ...slideStyles,
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+          background: 'white',
+          willChange: 'transform'
+        }}
+      >
+        <Stack h="100%" w="100%">
+          <Flex flex={1} mih={0}>
+            <TabNavigator defaultValue={PageRoute.SCENE_2D_ASSETS_CHART} />
           </Flex>
-        )}
-      </Stack>
-    </AnimatedDiv>
+
+          {!isMobile && (
+            <Flex flex={1} mih={0}>
+              <TabNavigator />
+            </Flex>
+          )}
+        </Stack>
+      </AnimatedDiv>
+    </div>
   );
 };
 
