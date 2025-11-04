8c394ed0c36a052a40eb9c90587c2ef2ad06607b
Salgac <domco.salgovic@gmail.com>
2025-11-04 16:21:05 +0100

    feat(visualizer-fe): Tweak content panels

--- apps/visualizer-fe/src/components/LeftPanel.tsx
+++ apps/visualizer-fe/src/components/LeftPanel.tsx
@@ -1,4 +1,6 @@
 import { Group } from '@mantine/core';
+import type { SpringValue } from '@react-spring/web';
+import { animated } from '@react-spring/web';
 
 import { useSelectedParachain } from '../context/SelectedParachain/useSelectedParachain';
 import Scene3d from '../pages/Scene3d';
@@ -6,15 +8,31 @@ import ChannelAlertContainer from './ChannelInfo/ChannelAlert.container';
 import EcosystemSelectContainer from './EcosystemSelect/EcosystemSelect.container';
 import Footer from './Footer/Footer';
 
-const LeftPanel = () => {
+const AnimatedGroup = animated(Group);
+
+interface LeftPanelProps {
+  animatedStyle: {
+    leftWidth: SpringValue<string>;
+  };
+}
+
+const LeftPanel = ({ animatedStyle }: LeftPanelProps) => {
   const { selectedChannel } = useSelectedParachain();
+
   return (
-    <Group flex={1} w="60%" h="100%" pos="relative">
+    <AnimatedGroup
+      h="100%"
+      pos="relative"
+      style={{
+        width: animatedStyle.leftWidth,
+        minWidth: 0
+      }}
+    >
       <Scene3d />
       <Footer />
       {selectedChannel && <ChannelAlertContainer selectedChannel={selectedChannel} />}
       <EcosystemSelectContainer />
-    </Group>
+    </AnimatedGroup>
   );
 };
 
