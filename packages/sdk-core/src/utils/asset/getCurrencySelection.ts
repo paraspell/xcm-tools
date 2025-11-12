4619da83cf5fdc7ea384b3d6b2decd1d8146f88c
Michael Absolon <79721475+michaeldev5@users.noreply.github.com>
2025-11-12 18:09:02 +0100

    fix(sdk): Fix e2e errors 🔧

--- packages/sdk-core/src/utils/asset/getCurrencySelection.ts
+++ packages/sdk-core/src/utils/asset/getCurrencySelection.ts
@@ -1,10 +1,11 @@
 import type { TAssetInfo, TCurrencyCore } from '@paraspell/assets'
 import { isForeignAsset } from '@paraspell/assets'
+import type { TSubstrateChain } from '@paraspell/sdk-common'
 
-export const getCurrencySelection = (asset: TAssetInfo): TCurrencyCore => {
+export const getCurrencySelection = (chain: TSubstrateChain, asset: TAssetInfo): TCurrencyCore => {
   if (asset.location) return { location: asset.location }
 
-  if (isForeignAsset(asset) && asset.assetId) {
+  if (isForeignAsset(asset) && asset.assetId && !chain.startsWith('Bifrost')) {
     return { id: asset.assetId }
   }
 
