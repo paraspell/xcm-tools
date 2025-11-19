8e5df0de9e9a9ff4be86bd5738c3bf85d9095e9f
Michael Absolon <79721475+michaeldev5@users.noreply.github.com>
2025-11-19 13:17:25 +0100

    fix(assets): Add missing Hydration assets ⚙️

--- packages/assets/scripts/fetchHydrationAssets.ts
+++ packages/assets/scripts/fetchHydrationAssets.ts
@@ -1,7 +1,29 @@
 import type { ApiPromise } from '@polkadot/api'
 import type { TForeignAssetInfo } from '../src'
 import { capitalizeLocation } from './utils'
-import { TSubstrateChain } from '@paraspell/sdk-common'
+import { getJunctionValue, hasJunction, TLocation, TSubstrateChain } from '@paraspell/sdk-common'
+import { createChainClient } from '../../sdk-pjs/src'
+import { getParaId, getRelayChainOf } from '../../sdk-core/src'
+
+const ALLOWED_AH_ASSET_SYMBOLS = ['BILL']
+
+const resolveAhMetadata = async (
+  location: TLocation,
+  ahApi: ApiPromise
+): Promise<{ symbol: string; decimals: number } | null> => {
+  const id = getJunctionValue(location, 'GeneralIndex')
+  const ahMetadataRaw = await ahApi.query.assets.metadata(id)
+  const ahMetadata = ahMetadataRaw.toHuman() as any
+
+  const symbol = ahMetadata.symbol
+  const decimals = +ahMetadata.decimals
+
+  if (!ALLOWED_AH_ASSET_SYMBOLS.includes(symbol)) {
+    return null
+  }
+
+  return { symbol, decimals }
+}
 
 export const fetchHydrationAssets = async (
   chain: TSubstrateChain,
@@ -10,6 +32,10 @@ export const fetchHydrationAssets = async (
 ): Promise<TForeignAssetInfo[]> => {
   const [module, method] = query.split('.')
   const response = await api.query[module][method].entries()
+
+  const ahChain = `AssetHub${getRelayChainOf(chain)}` as TSubstrateChain
+
+  const ahApi = await createChainClient(ahChain)
 
   const assets = await Promise.all(
     response.map(
@@ -26,14 +52,33 @@ export const fetchHydrationAssets = async (
         // Special case: overwrite PAS symbol for HydrationPaseo chain and assetId 5
         const finalSymbol = chain === 'HydrationPaseo' && numberAssetId === '5' ? 'PAS' : symbol
 
-        const location = await api.query.assetRegistry.assetLocations(era)
+        const locationRaw = await api.query.assetRegistry.assetLocations(era)
+
+        const location =
+          locationRaw.toJSON() !== null ? capitalizeLocation(locationRaw.toJSON()) : undefined
+
+        let resultSymbol = finalSymbol ?? ''
+        let resultDecimals = +decimals
+
+        const isAhAsset =
+          location &&
+          hasJunction(location, 'GeneralIndex') &&
+          getJunctionValue(location, 'Parachain') === getParaId(ahChain)
+
+        if (isAhAsset) {
+          const ahResolved = await resolveAhMetadata(location, ahApi)
+          if (ahResolved) {
+            resultSymbol = ahResolved.symbol
+            resultDecimals = ahResolved.decimals
+          }
+        }
 
         return {
           assetId: numberAssetId,
-          symbol: finalSymbol ?? '',
-          decimals: +decimals,
+          symbol: resultSymbol,
+          decimals: resultDecimals,
           existentialDeposit,
-          location: location.toJSON() !== null ? capitalizeLocation(location.toJSON()) : undefined
+          location
         }
       }
     )
