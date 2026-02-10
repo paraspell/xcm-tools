bad8c3d2b554e6d65b95d8193fa0c6ab343297c5
Michael Absolon <79721475+michaeldev5@users.noreply.github.com>
2026-02-10 14:12:25 +0100

    chore(xcm-router): Fix linter issues 🔧

--- packages/xcm-router/src/utils/isPjsExtrinsic.ts
+++ packages/xcm-router/src/utils/isPjsExtrinsic.ts
@@ -2,17 +2,14 @@ import type { Extrinsic } from '@paraspell/sdk-pjs';
 
 import type { TExtrinsic } from '../types';
 
-export function isPjsExtrinsic(tx: TExtrinsic): tx is Extrinsic {
-  return (
-    typeof tx === 'object' &&
-    tx !== null &&
-    'send' in tx &&
-    typeof tx.send === 'function' &&
-    'signAndSend' in tx &&
-    typeof tx.signAndSend === 'function' &&
-    'signAsync' in tx &&
-    typeof tx.signAsync === 'function' &&
-    'paymentInfo' in tx &&
-    typeof tx.paymentInfo === 'function'
-  );
-}
+export const isPjsExtrinsic = (tx: TExtrinsic): tx is Extrinsic =>
+  typeof tx === 'object' &&
+  tx !== null &&
+  'send' in tx &&
+  typeof tx.send === 'function' &&
+  'signAndSend' in tx &&
+  typeof tx.signAndSend === 'function' &&
+  'signAsync' in tx &&
+  typeof tx.signAsync === 'function' &&
+  'paymentInfo' in tx &&
+  typeof tx.paymentInfo === 'function';
