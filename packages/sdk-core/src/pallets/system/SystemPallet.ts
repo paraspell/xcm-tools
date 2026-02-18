dfb9c189cc518a3b7b72712bd194af5d28d2aae3
Michael Absolon <79721475+michaeldev5@users.noreply.github.com>
2026-02-18 11:04:09 +0100

    fix(sdk-core): Fix wh assets minting on Moonbeam 🔧

--- packages/sdk-core/src/pallets/system/SystemPallet.ts
+++ packages/sdk-core/src/pallets/system/SystemPallet.ts
@@ -8,11 +8,13 @@ import { assertHasId, formatAssetIdToERC20 } from '../../utils'
 
 const SIZE = 32
 const BALANCE_SLOT = 0
+const BALANCE_SLOT_WORMHOLE = 5
 
-const calculateMappingSlot = (key: string) => {
+const calculateMappingSlot = (key: string, assetId: string | undefined) => {
   const normalizedKey = getAddress(key)
   const keyPadded = pad(normalizedKey, { size: SIZE })
-  const slotHex = pad(toHex(BALANCE_SLOT), { size: SIZE })
+  const resolvedSlot = assetId?.startsWith('0x') ? BALANCE_SLOT_WORMHOLE : BALANCE_SLOT
+  const slotHex = pad(toHex(resolvedSlot), { size: SIZE })
   const encoded = concat([keyPadded, slotHex])
   return keccak256(encoded)
 }
@@ -27,7 +29,7 @@ export class SystemPallet extends BaseAssetsPallet {
   ): Promise<TSetBalanceRes> {
     assertHasId(assetInfo)
     const contractAddr = formatAssetIdToERC20(assetInfo.assetId)
-    const slot = calculateMappingSlot(address)
+    const slot = calculateMappingSlot(address, assetInfo.assetId)
     const amountEncoded = pad(toHex(assetInfo.amount + balance), { size: SIZE })
 
     const storageKey = await api.getEvmStorage(contractAddr, slot)
