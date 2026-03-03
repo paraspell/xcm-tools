ad771a0d763c25bb1099e34c058b19f7ece9b772
Michael Absolon <79721475+michaeldev5@users.noreply.github.com>
2026-03-03 10:55:47 +0100

    fix(xcm-router): Add missing generic param 🔧

--- packages/xcm-router/src/types/TRouterDryRun.ts
+++ packages/xcm-router/src/types/TRouterDryRun.ts
@@ -1,16 +1,10 @@
 import type { TDryRunChainResult, TDryRunResult, THopInfo } from '@paraspell/sdk';
 
-import type { TWithExchange } from './TRouter';
-
 /** @deprecated - Use TDryRunChainResult instead. Will be removed in v13. */
-export type TRouterDryRunChainResult = TWithExchange<TDryRunChainResult>;
+export type TRouterDryRunChainResult = TDryRunChainResult;
 
 /** @deprecated - Use THopInfo instead. Will be removed in v13. */
-export type TRouterDryRunHopInfo = TWithExchange<THopInfo>;
+export type TRouterDryRunHopInfo = THopInfo;
 
 /** @deprecated - Use TDryRunResult instead. Will be removed in v13. */
-export type TRouterDryRunResult = Omit<TDryRunResult, 'origin' | 'destination' | 'hops'> & {
-  origin: TRouterDryRunChainResult;
-  destination?: TRouterDryRunChainResult;
-  hops: TRouterDryRunHopInfo[];
-};
+export type TRouterDryRunResult = TDryRunResult;
--- packages/xcm-router/src/types/TXcmFee.ts
+++ packages/xcm-router/src/types/TXcmFee.ts
@@ -1,16 +1,10 @@
 import type { TGetXcmFeeResult, TXcmFeeDetail, TXcmFeeHopInfo } from '@paraspell/sdk';
 
-import type { TWithExchange } from './TRouter';
-
 /** @deprecated - Use TConditionalXcmFeeHopInfo instead. Will be removed in v13. */
-export type TRouterXcmFeeHopInfo = TWithExchange<TXcmFeeHopInfo>;
+export type TRouterXcmFeeHopInfo = TXcmFeeHopInfo;
 
 /** @deprecated - Use TConditionalXcmFeeDetail instead. Will be removed in v13. */
-export type TRouterXcmFeeDetail = TWithExchange<TXcmFeeDetail>;
+export type TRouterXcmFeeDetail = TXcmFeeDetail;
 
 /** @deprecated - Use TGetXcmFeeResult instead. Will be removed in v13. */
-export type TRouterXcmFeeResult = TGetXcmFeeResult & {
-  origin: TRouterXcmFeeDetail;
-  destination: TRouterXcmFeeDetail;
-  hops: TRouterXcmFeeHopInfo[];
-};
+export type TRouterXcmFeeResult = TGetXcmFeeResult;
